17368926d5a7656d84b0e5ffb711e2bf3d49b6b7
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-18 16:08:17 +0100

    Problemi con il ciclo for e il clone()

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 
+// definisco l'oggetto con gli elementi li di partenza.
+
+var activities = [
+"Connettersi alla classe Boolean alle 9:30",
+"Pranzo alle 13:15",
+"Fare l'esercizio sulla To Do list (sperando di non cannarlo): ADESSO"
+];
+
+
+for (var i = 0; i < activities.length; i++) {
+
+  var templateItem = $(".template-item").clone();
+  templateItem.prepend(activities[i]);
+  $(".to-do").append(templateItem);
+}
