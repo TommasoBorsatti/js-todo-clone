d1baafcaf0b2f0f6457d47266e64388353692043
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-18 16:34:13 +0100

    CSS - HTML : aggiunto campo Input

--- js/script.js
+++ js/script.js
@@ -7,7 +7,10 @@ var activities = [
 "Fare l'esercizio sulla To Do list (sperando di non cannarlo): ADESSO"
 ];
 
-
+// In un ciclo for definisco templateItem assegnando il li di template e clonandolo n volte quante è lungo l array delle attività.
+// a ogni clone vado a iniettare gli indici dell array activities.
+// Uso prepend in modo da non sovrascrivere le attività, ma da aggiungerle in modo consequenziale come primo elemento di li.
+// Seleziono l UL to-do e ci aggiungo con append l insieme dei li clonati (templateItem)
 for (var i = 0; i < activities.length; i++) {
 
   var templateItem = $(".template > li").clone();
